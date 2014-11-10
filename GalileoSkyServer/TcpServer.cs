7409354782718de705f7a13e8815f14b887cdccc
Sovietmade <ch3rkas0v.yuri@gmail.com>
2014-11-10 16:49:58 +0300

    initial

---
+++ GalileoSkyServer/TcpServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GalileoSkyServer
+{
+    class TcpServer
+    {
+        TcpListener mListener;
+
+        public TcpServer()
+        {
+            mListener = new TcpListener(IPAddress.Any, 5555);
+        }
+
+        public void BeginListenForIncomingConnections()
+        {
+            mListener.BeginAcceptTcpClient(DoAcceptTcpClientCallback, mListener);
+        }
+
+        public void DoAcceptTcpClientCallback(IAsyncResult ar)
+        {
+            // Get the listener that handles the client request.
+            TcpListener listener = (TcpListener)ar.AsyncState;
+
+            // End the operation and display the received data on  
+            // the console.
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+            //now create a TcpConnection wrapper around TcpClient,add it to connections list.
+
+            // Process the connection here. (Add the client to a 
+            // server table, read data, etc.)
+            Console.WriteLine("Client connected completed");
+        }
+
+    }
+}
